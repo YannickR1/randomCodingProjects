89bbbf62006df93a4bf545c6fa8ceea8c7ea9c9c
YannickR1 <yannickraats@gmail.com>
2022-10-18 01:24:15 +0200

    cooking project

--- cooking/cooking.js
+++ cooking/cooking.js
@@ -1,14 +1,35 @@
 // 1. Declare expected time in oven
-let EXPECTED_TIME_IN_OVEN = 40
+let EXPECTED_TIME_IN_OVEN = 40;
 
 // 2. Calculate remaining time in oven
-let remainingOvenTime 
+let remainingOvenTime;
 
- remainingTimeInMinutes = (i) => {
-  remainingOvenTime = EXPECTED_TIME_IN_OVEN - i
-  return remainingOvenTime
- }
+remainingTimeInMinutes = (i) => {
+  return (remainingOvenTime = EXPECTED_TIME_IN_OVEN - i);
+};
 
 // 3. Calculate prep time in minutes
+let prepTime;
+
+calcPrepTime = (i) => {
+  return (prepTime = i * 2);
+};
 
 // 4. Calculate total time in minutes
+
+let totalTimeInMinutes
+
+calcTotalTime = (i, j) => {
+  return i + j
+}
+
+
+// run the program
+
+run = () => {
+  console.log(remainingTimeInMinutes(10))
+
+  console.log(calcTotalTime(EXPECTED_TIME_IN_OVEN, calcPrepTime(5)))
+}
+
+run()
---
+++ tournament-sim/notes.md
@@ -0,0 +1,31 @@
+**Design Plan**
+  What are objects?
+  What are their attributes?
+  What are their behaviours? 
+  What are their collections? Or how are they grouped?
+  How do they interact?
+  What is the development schedule?
+
+**Notes**
+    Objects?
+      Teams?
+      League
+      Match (12 bouts between 2 teams)
+      Leaderboard
+      Schedule
+
+  **Attributes**
+      Of a team?
+        Has a name
+        Has points
+        Has match results
+      League
+        Consists of 20 teams
+        Every team plays 2 matches against other team (40 matches in total)
+      Match 
+        Can win (3 points), lose (0 points) or draw (1 point)
+      Leaderboard
+        Tracks all team attributes 
+        Sorts team attributes by points
+
+    

758769dd2699d5268428ff37b47dfc630057929e
YannickR1 <yannickraats@gmail.com>
2022-10-17 13:39:00 +0200

    git init

---
+++ cooking/cooking.js
@@ -0,0 +1,14 @@
+// 1. Declare expected time in oven
+let EXPECTED_TIME_IN_OVEN = 40
+
+// 2. Calculate remaining time in oven
+let remainingOvenTime 
+
+ remainingTimeInMinutes = (i) => {
+  remainingOvenTime = EXPECTED_TIME_IN_OVEN - i
+  return remainingOvenTime
+ }
+
+// 3. Calculate prep time in minutes
+
+// 4. Calculate total time in minutes
